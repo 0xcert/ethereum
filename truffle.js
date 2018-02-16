9309f40b6839f26c2b1e8fe5fb8ded3247ec8f8f
Luka Kacil <luka.kacil@gmail.com>
2018-02-16 23:40:29 +0100

    Add initial truffle config

--- truffle.js
+++ truffle.js
@@ -1,4 +1,16 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+  networks: {
+    rinkeby: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gas: 870000
+    },
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*', // Match any network id
+      gas: 4000000
+    }
+  }
+}
