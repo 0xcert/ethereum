5ad8dccb9b3b0cc2214bd6bfb3a65534d89b6415
xpeper <xpepermint@gmail.com>
2018-02-17 22:19:02 +0100

    removing migrations, Xcert test fix

--- migrations/2_initial_contracts.js
+++
@@ -1,7 +0,0 @@
-const XCTToken = artifacts.require("XCTToken");
-const xcert = artifacts.require("Xcert");
-
-module.exports = function(deployer) {
-  deployer.deploy(XCTToken);
-  deployer.deploy(xcert, 'test', 'TST');
-};
