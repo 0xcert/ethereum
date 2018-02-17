2f411b0081a083fc0f8fc3520be19f4d96db1f92
xpeper <xpepermint@gmail.com>
2018-02-17 19:34:42 +0100

    refactoring code style and tests for XcertToken

--- test/math/SafeMath.test.js
+++ test/math/SafeMath.test.js
@@ -1,15 +1,15 @@
+const SafeMathMock = artifacts.require('SafeMathMock');
 const assertRevert = require('../helpers/assertRevert');
 const assertJump = require('../helpers/assertJump');
-const SafeMathMock = artifacts.require('SafeMathMock');
 
-contract('SafeMath', function (accounts) {
+contract('SafeMath', (accounts) => {
   let safeMath;
 
-  before(async function () {
+  before(async () => {
     safeMath = await SafeMathMock.new();
   });
 
-  it('multiplies correctly', async function () {
+  it('multiplies correctly', async () => {
     let a = 5678;
     let b = 1234;
     await safeMath.multiply(a, b);
@@ -17,25 +17,23 @@ contract('SafeMath', function (accounts) {
     assert.equal(result, a * b);
   });
 
-  it('adds correctly', async function () {
+  it('adds correctly', async () => {
     let a = 5678;
     let b = 1234;
     await safeMath.add(a, b);
     let result = await safeMath.result();
-
     assert.equal(result, a + b);
   });
 
-  it('subtracts correctly', async function () {
+  it('subtracts correctly', async () => {
     let a = 5678;
     let b = 1234;
     await safeMath.subtract(a, b);
     let result = await safeMath.result();
-
     assert.equal(result, a - b);
   });
 
-  it('should throw an error if subtraction result would be negative', async function () {
+  it('should throw an error if subtraction result would be negative', async () => {
     let a = 1234;
     let b = 5678;
     try {
@@ -46,13 +44,13 @@ contract('SafeMath', function (accounts) {
     }
   });
 
-  it('should throw an error on addition overflow', async function () {
+  it('should throw an error on addition overflow', async () => {
     let a = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
     let b = 1;
     await assertRevert(safeMath.add(a, b));
   });
 
-  it('should throw an error on multiplication overflow', async function () {
+  it('should throw an error on multiplication overflow', async () => {
     let a = 115792089237316195423570985008687907853269984665640564039457584007913129639933;
     let b = 2;
     await assertRevert(safeMath.multiply(a, b));
--- test/ownership/Ownable.test.js
+++ test/ownership/Ownable.test.js
@@ -10,7 +10,6 @@ contract('Ownable', (accounts) => {
 
   it('has an owner', async () => {
     const owner = await ownable.owner();
-
     assert.isTrue(owner !== 0);
   });
 
@@ -18,21 +17,18 @@ contract('Ownable', (accounts) => {
     let other = accounts[1];
     await ownable.transferOwnership(other);
     let owner = await ownable.owner();
-
     assert.isTrue(owner === other);
   });
 
   it('prevents non-owners from transfering', async () => {
     const other = accounts[2];
     const owner = await ownable.owner.call();
-
     assert.isTrue(owner !== other);
     await assertRevert(ownable.transferOwnership(other, { from: other }));
   });
 
   it('guards ownership against stuck state', async () => {
     const originalOwner = await ownable.owner();
-    
     await assertRevert(ownable.transferOwnership(null, { from: originalOwner }));
   });
 });
